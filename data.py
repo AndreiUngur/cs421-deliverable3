00e02332773f9a83dddf6c98a645175621855514
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-03-25 17:39:35 -0400

    finished vis data export to csv

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import datetime
 import collections
+import csv
 from utils import db
 
 
@@ -12,10 +13,18 @@ def vis_reviews():
                     '<table><tr><th>Rating</th><th>Date</th>'
 
 
-    review_data = get_review_data() 
+    review_data = get_review_data()
+    review_writer = csv.writer(open('review.csv', 'w'), delimiter = ',')
+
+    user_data = get_user_data()
+    user_writer = csv.writer(open('user.csv', 'w'), delimiter = ',')
 
     for d in review_data:
+        review_writer.writerow(d)
+
+    for d in user_data:
         HTML_output += f"<tr><td>{d[0]}</td><td>{d[1]}</td>"
+        user_writer.writerow(d)
 
     HTML_output += "</body>"
     return HTML_output
@@ -23,6 +32,26 @@ def vis_reviews():
 def vis_users():
     return ""
 
+
+def get_user_data():
+    find_user_data_command = "SELECT username, creation_date FROM UserAccount;"
+    results = db.engine.execute(find_user_data_command)
+
+    date_count = {} # date -> count
+    for row in results:
+        username, date = row
+        date = date.isoformat()[:7] # we only care about year and month
+
+        if date not in date_count:
+            date_count[date] = 0
+
+        date_count[date] += 1
+
+    user_data = []
+    for date in sorted(date_count):
+        user_data.append([date, date_count[date]])
+
+    return user_data
 
 def get_review_data():
     find_review_data_command = "SELECT rating, date FROM reviews;"
